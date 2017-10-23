1ff04b934c474a3f949eabc41236811f56c6fff7
neenurose <neenuvincent1992@gmail.com>
2017-10-23 03:03:59 +0100

    server(with thread)-client(with thread) chat with group chat implementation

--- Chat_client.py
+++ Chat_client.py
@@ -1,5 +1,49 @@
 import socket
 import sys
+import threading
+from threading import Thread
+from socketserver import ThreadingMixIn
+
+class server_thread(Thread):
+    def __init__(self,c_socket):
+        Thread.__init__(self)
+        self.c_socket = c_socket
+
+    def run(self):
+        while True:
+            message_server = self.c_socket.recv(2048).decode()
+            if "Thank you" in message_server:
+                print(message_server)
+                flag=1
+                self.c_socket.close()
+                sys.exit()
+            else:
+                if len(message_server)>0:
+                    print(message_server)
+
+                sys.stdout.write("Type a Message: ")
+                sys.stdout.flush()
+                message_client = sys.stdin.readline()
+                self.c_socket.send(message_client.encode())
+
+class server_reply(Thread):
+    def __init__(self,c_socket):
+        Thread.__init__(self)
+        self.c_socket = c_socket
+
+    def run(self):
+        client_socket2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket2.connect((IP_address, Port2))
+        #print("test: inside thread reply")
+        while True:
+            if flag==0:
+                msg_from_server=client_socket2.recv(1024).decode()
+                print(msg_from_server)
+            if flag == 1:
+                client_socket2.close()
+                sys.exit()
+
+
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 if len(sys.argv) != 3:
@@ -8,18 +52,26 @@ if len(sys.argv) != 3:
 IP_address = str(sys.argv[1])
 Port = int(sys.argv[2])
 client_socket.connect((IP_address, Port))
+Port2 = 5050
+flag=0
+threads = []
 
-while True:
-    message_server = client_socket.recv(2048).decode()
-    if "Thank you" in message_server:
-        print(message_server)
-        client_socket.close()
+try:
+    clientThread = server_thread(client_socket)
+    clientThread.daemon = True
+    clientThread.start()
+
+    clientThread2 = server_reply(client_socket)
+    clientThread2.daemon = True
+    clientThread2.start()
+    threads.append(clientThread)
+    threads.append(clientThread2)
+    while True:
+        for t in threads:
+            t.join(600)
+            if not t.isAlive():
+                break
+        break
+except KeyboardInterrupt:
+        client_socket.send("Bye".encode())
         sys.exit()
-    else:
-        if len(message_server)>0:
-            print(message_server)
-
-        sys.stdout.write("Type a Message: ")
-        sys.stdout.flush()
-        message_client = sys.stdin.readline()
-        client_socket.send(message_client.encode())
--- Chat_server.py
+++ Chat_server.py
@@ -19,12 +19,65 @@ class client(Thread):
             client_message = self.client_socket.recv(2048).decode()
             print("From client "+str(self.client_ip)+":"+str(self.client_port)+": "+client_message)
             if "Bye" in client_message:
+                if len(s_queue.values())>1:
+                    msg_to_broadcast = "From client "+str(self.client_ip)+":"+str(self.client_port)+": "+client_message
+                    thread_lock.acquire()
+                    del s_queue[self.client_socket.fileno()]
+                    for key in s_queue.keys():
+                        #print(s_queue)
+                        if key != self.client_socket.fileno():
+                            q = s_queue[key]
+                            q.put(msg_to_broadcast)
+                    thread_lock.release()
+                    #self.client_socket.send(("From server: Broadcasted").encode())
+                else:
+                    thread_lock.acquire()
+                    del s_queue[self.client_socket.fileno()]
+                    thread_lock.release()
+
                 self.client_socket.send(("Thank you for connecting").encode())
                 break;
                 #self.client_socket.close()
                 #sys.exit()
             else:
-                self.client_socket.send(("from server: "+client_message).encode())
+                #print(len(s_queue.values()))
+                if len(s_queue.values())>1:
+                    msg_to_broadcast = "\nFrom client "+str(self.client_ip)+":"+str(self.client_port)+": "+client_message
+                    thread_lock.acquire()
+                    for key in s_queue.keys():
+                        #print(s_queue)
+                        if key != self.client_socket.fileno():
+                            q = s_queue[key]
+                            q.put(msg_to_broadcast)
+                    thread_lock.release()
+                    self.client_socket.send(("Me: "+client_message).encode())
+                else:
+                    msg_to_send = "Me: "+client_message
+                    #thread_lock.acquire()
+                    #s_queue[self.client_socket.fileno()].put(msg_to_send)
+                    #thread_lock.release()
+                    self.client_socket.send(msg_to_send.encode())
+
+
+class client_reply(Thread):
+    def __init__(self,client_socket):
+        Thread.__init__(self)
+        self.client_socket = client_socket;
+        #print("hello")
+
+    def run(self):
+        server_socket2.listen(1)
+        (client_soc,(client_ip_addr, client_port_num))=server_socket2.accept()
+        #print(self.client_socket.fileno())
+        while True:
+            try:
+                msg = s_queue[self.client_socket.fileno()].get(False)
+                print(msg)
+                client_soc.send(msg.encode())
+            except queue.Empty:
+                msg = "no message"
+            except KeyError as e:
+                pass
 
 
 thread_lock = threading.Lock()
@@ -41,15 +94,25 @@ port = int(sys.argv[1])
 server_socket.bind(('',port))
 server_socket.listen(5)
 
+server_socket2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket2.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+port2 = 5050
+server_socket2.bind(('',port2))
+
+
 while True:
     print("Server is ready and listening...")
     (client_socket,(client_ip,client_port)) = server_socket.accept()
     q = queue.Queue()
     thread_lock.acquire()
 
-    s_queue[client_socket.fileno()] = queue
+    s_queue[client_socket.fileno()] = q
     thread_lock.release()
 
-    newthread = client(client_socket,client_ip,client_port)
-    newthread.daemon = True
-    newthread.start()
+    client_thread = client(client_socket,client_ip,client_port)
+    client_thread.daemon = True
+    client_thread.start()
+
+    client_thread2 = client_reply(client_socket)
+    client_thread2.daemon = True
+    client_thread2.start()
